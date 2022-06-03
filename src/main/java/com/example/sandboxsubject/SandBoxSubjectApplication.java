45baffc91d963173774277439e23626beb7008a5
SMJ <smj@SMJs-MacBook-Pro.local>
2022-06-03 19:53:48 +0900

    Spring boot Project Setting

---
+++ src/main/java/com/example/sandboxsubject/SandBoxSubjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sandboxsubject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandBoxSubjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandBoxSubjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sandboxsubject/SandBoxSubjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sandboxsubject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandBoxSubjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
